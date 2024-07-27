b199e64ae25b10e167f1329dad5085765153a56c
BoomPartner <soporte@boom-partner.com>
2024-07-26 20:56:47 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import Navbar from './components/Navbar.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
-
- 
-    <Navbar></Navbar>
+<React.StrictMode>
     <App />
-   
- 
+  </React.StrictMode>,
 )
